fdeb2e718163c01360ee1ebabdbdbeaa0f491b5b
karendam <41386369+karendam@users.noreply.github.com>
2018-08-08 10:17:50 -0700

    Update sleepBetter.py

--- sleepBetter.py
+++ sleepBetter.py
@@ -40,6 +40,8 @@ def timer():
     return milliseconds
 
 def newtimer():
+    newtop=Toplevel()
+    newinput=Entry(newtop)
     newtop.attributes("-topmost", "true")
     newtop.title('Timer')
     newlabel=Label(newtop, text = "How many minutes do you want?")
@@ -78,9 +80,10 @@ def rainStart():
     pygame.mixer.init()
     sounda= pygame.mixer.Sound("rain-03.wav")
     milliseconds=timer()
-    sounda.play(-1,milliseconds)
+    if milliseconds != 0:
+        sounda.play(-1,milliseconds)
+    sounda.play(-1,newmilliseconds)
     print("Timer Initiated")
-
 
 def paperStart():
     milliseconds = 0
@@ -88,7 +91,9 @@ def paperStart():
     pygame.mixer.init()
     soundb= pygame.mixer.Sound("paper-rustle-5.wav")
     milliseconds=timer()
-    soundb.play(-1,milliseconds)
+    if milliseconds != 0:
+        soundb.play(-1,milliseconds)
+    soundb.play(-1,newmilliseconds)    
     print("Timer Initiated")
 
 def planeStart():
@@ -97,7 +102,9 @@ def planeStart():
     pygame.mixer.init()
     soundc= pygame.mixer.Sound("airplane-interior-1.wav")
     milliseconds=timer()
-    soundc.play(-1,milliseconds)
+    if milliseconds != 0:
+        soundc.play(-1,milliseconds)
+    soundc.play(-1,newmilliseconds)    
     print("Timer Initiated")
 
 def fanStart():
@@ -106,16 +113,20 @@ def fanStart():
     pygame.mixer.init()
     soundd= pygame.mixer.Sound("18-Fan-10min.wav")
     milliseconds=timer()
-    soundd.play(-1,milliseconds)
+    if milliseconds != 0:
+        soundd.play(-1,milliseconds)
+    soundd.play(-1,newmilliseconds)    
     print("Timer Initiated")
-
+    
 def clockStart():
     milliseconds = 0
     pygame.init()
     pygame.mixer.init()
     sounde= pygame.mixer.Sound("clock-ticking-2.wav")
     milliseconds=timer()
-    sounde.play(-1,milliseconds)
+    if milliseconds != 0:
+        sounde.play(-1,milliseconds)
+    sounde.play(-1,newmilliseconds)    
     print("Timer Initiated")
 
 def trainStart():
@@ -124,16 +135,20 @@ def trainStart():
     pygame.mixer.init()
     soundf= pygame.mixer.Sound("train-pass-by-02.wav")
     milliseconds=timer()
-    soundf.play(-1,milliseconds)
+    if milliseconds != 0:
+        soundf.play(-1,milliseconds)
+    soundf.play(-1,newmilliseconds)    
     print("Timer Initiated")
-
+    
 def carStart():
     milliseconds = 0
     pygame.init()
     pygame.mixer.init()
     soundg= pygame.mixer.Sound("car-passing-1.wav")
     milliseconds=timer()
-    soundg.play(-1,milliseconds)
+    if milliseconds != 0:
+        soundg.play(-1,milliseconds)
+    soundg.play(-1,newmilliseconds)    
     print("Timer Initiated")
 
 def dropStart():
@@ -142,7 +157,9 @@ def dropStart():
     pygame.mixer.init()
     soundh= pygame.mixer.Sound("water-dripping-1.wav")
     milliseconds=timer()
-    soundh.play(-1,milliseconds)
+    if milliseconds != 0:
+        soundh.play(-1,milliseconds)
+    soundh.play(-1,newmilliseconds)    
     print("Timer Initiated")
 
 def oceanStart():
@@ -151,7 +168,9 @@ def oceanStart():
     pygame.mixer.init()
     soundi= pygame.mixer.Sound("ocean-wave-2.wav")
     milliseconds=timer()
-    soundi.play(-1,milliseconds)
+    if milliseconds != 0:
+        soundi.play(-1,milliseconds)
+    soundi.play(-1,newmilliseconds)    
     print("Timer Initiated")
 
 def windStart():
@@ -160,7 +179,9 @@ def windStart():
     pygame.mixer.init()
     soundj= pygame.mixer.Sound("wind-1.wav")
     milliseconds=timer()
-    soundj.play(-1,milliseconds)
+   if milliseconds != 0:
+        soundj.play(-1,milliseconds)
+    soundj.play(-1,newmilliseconds)    
     print("Timer Initiated")
 
 def riverStart():
@@ -169,7 +190,9 @@ def riverStart():
     pygame.mixer.init()
     soundk= pygame.mixer.Sound("river.wav")
     milliseconds=timer()
-    soundk.play(-1,milliseconds)
+   if milliseconds != 0:
+        soundk.play(-1,milliseconds)
+    soundk.play(-1,newmilliseconds)    
     print("Timer Initiated")
 
 fillFrame = Frame(root, width = 50, height = 50, bd= 1)
